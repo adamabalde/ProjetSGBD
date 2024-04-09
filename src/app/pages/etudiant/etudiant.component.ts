7512398fd78af5c386ceea7c1c1d3b6cdf7fcbde
NDIAYE Adja <adja.fatou.ndiaye.pro@gmail.com>
2024-04-09 20:46:53 +0000

    modifs finales

--- src/app/pages/etudiant/etudiant.component.ts
+++ src/app/pages/etudiant/etudiant.component.ts
@@ -16,6 +16,9 @@ export class EtudiantComponent implements OnInit {
   userData: any = {}; // Propriété pour stocker les données de l'utilisateur
   coursList: any[] = []; // Propriété pour stocker la liste des cours
   isEtudiantResponsable: boolean = false; // Nouvelle propriété pour déterminer si l'utilisateur est un étudiant responsable
+  selectedCoursId: number | null = null; // Propriété pour stocker l'ID du cours sélectionné par l'utilisateur
+  avis = ''; // Propriété pour stocker le contenu de l'avis
+  avisEtudiantList: any[] = [];
 
   constructor(
     private authService: AuthentificationService,
@@ -52,8 +55,39 @@ export class EtudiantComponent implements OnInit {
 
     console.log('Données utilisateur récupérées dans le service :', this.userData);
   }
+  envoyerAvis(): void {
+    if (!this.selectedCoursId || !this.avis) {
+      console.error('Veuillez sélectionner un cours et écrire votre avis.');
+      return;
+    }
 
-  changeContent(menuItem: string): void {
+    const idCours = this.selectedCoursId;
+    const avis = this.avis;
+    const idEtudiant = this.userData.id; // Récupération de l'idEtudiant depuis les données de l'utilisateur
+
+    this.coursService.ajouterAvis(idCours, avis, idEtudiant).subscribe(
+      (response) => {
+        console.log('Avis ajouté avec succès:', response);
+        // Réinitialiser les champs du formulaire après l'ajout de l'avis
+        this.selectedCoursId = null;
+        this.avis = '';
+      },
+      (error) => {
+        console.error('Erreur lors de l\'ajout de l\'avis:', error);
+      }
+    );
+  }
+
+
+  getAvisEtudiants(): void {
+    this.coursService.getAvisEtudiants().subscribe(avisEtudiantList => {
+      console.log('Avis des étudiants récupérés:', avisEtudiantList);
+      this.avisEtudiantList = avisEtudiantList;
+    });
+  }
+
+
+  changeContent(menuItem: string , idCours?: number): void {
     if (menuItem === 'accueil') {
       this.pageTitle = 'Accueil';
       this.pageContent = 'Bienvenue dans votre tableau de bord.';
@@ -64,16 +98,29 @@ export class EtudiantComponent implements OnInit {
     } else if (menuItem === 'recueillir_avis' && this.isEtudiantResponsable) {
       this.pageTitle = 'Recueillir avis';
       this.pageContent = 'Collectez les avis des étudiants sur nos cours en vous rendant chez votre Responsable pédagogique pour obtenir les formulaires à soumettre aux étudiants. Une fois remplis, veuillez les lui retourner.'
-
       this.userData = {};
-      this.coursList = [];
 
-    } else if (menuItem === 'avis') {
+      this.getAvisEtudiants();
+
+
+
+    }
+
+
+
+     else if (menuItem === 'avis') {
+
       this.pageTitle = 'Donner avis';
-      this.pageContent = 'Le responsable de classe vous remettra un formulaire à la fin de chaque module pour vos impressions.';
+      this.pageContent = 'Sélectionnez le cours sur lequel vous voulez donner votre avis.';
+
       this.userData = {};
       this.coursList = [];
+      this.selectedCoursId = null; // Réinitialiser l'ID du cours sélectionné
 
+      this.coursService.getAllCours().subscribe(cours => {
+        console.log('Cours récupérés :', cours);
+        this.coursList = cours;
+      });
     }
     else if (menuItem === 'cours') {
       this.pageTitle = 'Cours';
--- src/app/services/cours.service.ts
+++ src/app/services/cours.service.ts
@@ -18,6 +18,14 @@ export class CoursService {
     return this.http.get<any[]>(`${this.apiUrl}/enseignant/${idEnseignant}/cours`);
   }
 
+  ajouterAvis(idCours: number, avis: string, idEtudiant: number): Observable<any> {
+    const url = `${this.apiUrl}/avis`;
+    const body = { idCours, avis, idEtudiant }; // Ajout de l'idEtudiant dans le corps de la requête
+    return this.http.post<any>(url, body);
+  }
+  getAvisEtudiants(): Observable<any[]> {
+    const url = `${this.apiUrl}/avisEtudiants`;
+    return this.http.get<any[]>(url);
+  }
 
-  // Autres méthodes pour la gestion des cours si nécessaire
 }
